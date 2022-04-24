7a0f78ee0f51be04c7c43a3e21efc4c5a4db4ab7
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2022-04-24 16:50:23 -0700

    Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : MonoBehaviour
     GameObject focalPoint;
     Renderer rendererPlayer;
     public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
+    public GameObject PowerUpIndicator;
+
+    bool hasPowerUp = false;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -28,8 +33,6 @@ public class PlayerController : MonoBehaviour
         float magnitude = forwardInput * speed * Time.deltaTime;
         rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
 
-        Debug.Log("Mag:" + magnitude);
-        Debug.Log("FI:" + forwardInput);
 
         if(forwardInput > 0)
         {
@@ -40,5 +43,45 @@ public class PlayerController : MonoBehaviour
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f - forwardInput);
         }
 
+        PowerUpIndicator.transform.position = transform.position;
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdown());
+            PowerUpIndicator.SetActive(true);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collid with" + collision.gameObject + "with powerup set to: " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+
+        }
+
+    }
+
+    IEnumerator PowerUpCountdown()
+
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        PowerUpIndicator.SetActive(false);
+    }
+
+
+
 }
